cf2b8147709cca9baf0cd92039a39021249b7597
BuildTools <finnian@fxapi.co.uk>
2016-01-15 18:55:27 +0000

    Include controller.py

---
+++ controller.py
@@ -0,0 +1,11 @@
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+from controlroom import ControlRoom
+
+mc = minecraft.Minecraft.create()
+cr = ControlRoom(mc)
+
+mc.postToChat("We're hunting one of you down...")
+duration = cr.chase(block.OBSIDIAN.id, block.GLOWING_OBSIDIAN.id, randomplayer=True, threat="water")
+print("You lasted %i seconds!" % duration)
+mc.postToChat("You lasted %i seconds!" % duration)
