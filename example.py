b52020b9d44ff5a1ec671cd0c9a0961c109f3600
BuildTools <finnian@fxapi.co.uk>
2016-01-15 19:58:23 +0000

    Rename controller.py to example.py

--- example.py
+++ example.py
@@ -6,6 +6,6 @@ mc = minecraft.Minecraft.create()
 cr = ControlRoom(mc)
 
 mc.postToChat("We're hunting one of you down...")
-duration = cr.chase(block.OBSIDIAN.id, block.GLOWING_OBSIDIAN.id, randomplayer=True, threat="water")
+duration = cr.chase(block.OBSIDIAN.id, block.GLOWING_OBSIDIAN.id, randomplayer=True, threat="lava")
 print("You lasted %i seconds!" % duration)
 mc.postToChat("You lasted %i seconds!" % duration)
